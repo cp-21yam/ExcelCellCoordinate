f70f8ca40aaea9a3512a16a49036fab363694044
Monnerat Yanis <Yanis.Monnerat@ceff.ch>
2024-05-03 08:13:30 +0200

    test

--- cp-21yamExcelCoordinate.build.ps1
+++ cp-21yamExcelCoordinate.build.ps1
@@ -1,6 +1,6 @@
 task Test {
-    Import-Module Pester
-    Invoke-Pester .\cp-21yamExcelCoordinate.Tests.ps1
+    # Import-Module Pester
+    # Invoke-Pester .\cp-21yamExcelCoordinate.Tests.ps1
 
     Import-Module PSScriptAnalyzer
     Invoke-ScriptAnalyzer -Path .\cp-21yamExcelCoordinate.psm1
