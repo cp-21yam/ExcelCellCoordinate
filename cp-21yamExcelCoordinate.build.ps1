6a69a62d35f04ca7ff13e7432200830515cf5c9d
Monnerat Yanis <Yanis.Monnerat@ceff.ch>
2024-05-03 08:14:15 +0200

    test

--- cp-21yamExcelCoordinate.build.ps1
+++ cp-21yamExcelCoordinate.build.ps1
@@ -1,10 +1,10 @@
-task Test {
-    # Import-Module Pester
-    # Invoke-Pester .\cp-21yamExcelCoordinate.Tests.ps1
+# task Test {
+#     # Import-Module Pester
+#     # Invoke-Pester .\cp-21yamExcelCoordinate.Tests.ps1
 
-    Import-Module PSScriptAnalyzer
-    Invoke-ScriptAnalyzer -Path .\cp-21yamExcelCoordinate.psm1
-}
+#     Import-Module PSScriptAnalyzer
+#     Invoke-ScriptAnalyzer -Path .\cp-21yamExcelCoordinate.psm1
+# }
 task Release {
     Set-PsEnv
     Publish-PSResource -Path .\cp-21yamExcelCoordinate.psd1 -Repository PSGallery -ApiKey $env:PSGALLERY_API_KEY
